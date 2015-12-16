c5716d072b638b94c9376552c6a4bf40606bd98e
xubowei <xubowei96@163.com>
2015-12-16 13:32:56 +0800

    最长子序列和矩阵连乘优化已完成

---
+++ MatrixChain/MatrixChain/main.cpp
@@ -0,0 +1,79 @@
+//
+//  main.cpp
+//  MatrixChain
+//
+//  Created by 徐 BOWEI on 15/12/15.
+//  Copyright © 2015年 徐 BOWEI. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+string str = "";
+int n = 0;
+typedef int TwoDimArray[ 5 ][ 5 ];
+void OptMatrix( const int C[ ], int N,
+          TwoDimArray M, TwoDimArray lastChange )
+{
+    int i, k, Left, Right;
+    int ThisM;
+    
+    for( Left = 1; Left <= N; Left++ )
+        M[ Left ][ Left ] = 0;
+    for( k = 1; k < N; k++ )
+        for( Left = 1; Left <= N - k; Left++ )
+        {
+            Right = Left + k;
+            M[ Left ][ Right ] = INT16_MAX;
+            for( i = Left; i < Right; i++ )
+            {
+                ThisM = M[ Left ][ i ] + M[ i + 1 ][ Right ]
+                + C[ Left - 1 ] * C[ i ] * C[ Right ];
+                if( ThisM < M[ Left ][ Right ] )
+                {
+                    M[ Left ][ Right ] = ThisM;
+                    lastChange[ Left ][ Right ] = i;
+                }
+            }
+        }
+}
+
+void FindTheWay(TwoDimArray lastChange,int start, int end)
+{
+    int X = lastChange[start][end];
+    if (!X) {
+        cout<<str[start - 1];
+    }else {
+        cout<<"(";
+        FindTheWay(lastChange, start, X);
+        FindTheWay(lastChange, X + 1, end);
+        cout<<")";
+    }
+    
+}
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int C[ ] = { 50, 10, 40, 30, 5 };
+    for (char c = 'A'; c < 'A' + sizeof(C)/sizeof(int) - 1; c++) {
+        str.push_back(c);
+    }
+    int M[ 5 ][ 5 ] = {{0}}, LastChange[ 5 ][ 5 ] = {{0}};
+    int i, j;
+    OptMatrix( C, 4, M, LastChange );
+       for( i = 1; i <= 4; i++ )
+    {
+        for( j = 1; j <= 4; j++ )
+            printf( "%14d", M[ i ][ j ] );
+        printf( "\n" );
+    }
+    printf("***********************\n");
+    for( i = 1; i <= 4; i++ )
+    {
+        for( j = 1; j <= 4; j++ )
+            printf( "%14d", LastChange[ i ][ j ] );
+        printf( "\n" );
+    }
+    cout<<str<<endl;
+    FindTheWay(LastChange, 1, 4);
+    return 0;
+}
---
+++ MaxSonString/MaxSonString/main.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  MaxSonString
+//
+//  Created by 徐 BOWEI on 15/12/16.
+//  Copyright © 2015年 徐 BOWEI. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+#define SIZE 10
+void MaxSonString(string str)
+{
+    int N = (int)str.size();
+    int MAX = 1;
+    vector<int>lengthOfEachChar(N,0);//用于存放f(i)值；
+    lengthOfEachChar[0] = 1;//以第a1为末元素的最长递增子序列长度为1；
+    for(int i = 1;i<N;i++)//循环n-1次
+    {
+        lengthOfEachChar[i]=1;//f[i]的最小值为1；
+        for(int j=0;j<i;j++)//循环i 次
+        {
+            if(str[j]<str[i]&&lengthOfEachChar[j]>lengthOfEachChar[i]-1)
+                lengthOfEachChar[i]=lengthOfEachChar[j]+1;//更新f[i]的值。
+            MAX = lengthOfEachChar[i] > MAX ? lengthOfEachChar[i] : MAX;
+        }
+    }
+    cout<<MAX<<endl;
+}
+
+
+int main(int argc, const char * argv[]) {
+    string str = "abcacacacacacacabcdefg";
+    MaxSonString(str);
+    return 0;
+}
